17ab22b04e3278d36992b242cc197e191f767fbc
Thomas Sandberg <thomas@sandberg.cloud>
2023-02-09 14:11:53 +0100

    Added multiple choices

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,21 @@
-cd /workspaces/wagono-ptxdist-config-cc100/src/
-ptxdist select configs/cc100/ptxconfig_generic
-ptxdist platform configs/cc100/platformconfig
-ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin
-ptxdist go -q
+if [ -z "$1" ]
+then
+    echo "Platform not specified! Choices: cc100, pfc or tp"
+elif [ $1 = "cc100" ]
+then
+    echo "CC100 selected"
+    echo "**************"
+    cd /workspaces/wagono-ptxdist-config-cc100/src/
+    ptxdist select configs/cc100/ptxconfig_generic
+    ptxdist platform configs/cc100/platformconfig
+    ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin
+    ptxdist go -q
+elif [ $1 = "pfc" ]
+then
+    echo "PFC selected"
+elif [ $1 = "tp" ]
+then
+    echo "TP/Edge selected"
+else
+    echo "Unknown platform! Choices: cc100, pfc or tp"
+fi

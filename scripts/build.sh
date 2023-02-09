f34bafd2d7dbac6c7aaf23a2c2535c9e9e4cf92f
Thomas Sandberg <thomas@sandberg.cloud>
2023-02-09 09:51:16 +0000

    updated script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-cd cd /workspaces/wagono-ptxdist-config-cc100/src/
+cd /workspaces/wagono-ptxdist-config-cc100/src/
 ptxdist select configs/cc100/ptxconfig_generic
 ptxdist platform configs/cc100/platformconfig
 ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin

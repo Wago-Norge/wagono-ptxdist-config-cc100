5c0675541e0cd57a04f3108c19cf8dc54c005782
Thomas Sandberg <thomas@sandberg.cloud>
2023-02-09 09:47:09 +0000

    added initial build script

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+cd cd /workspaces/wagono-ptxdist-config-cc100/src/
+ptxdist select configs/cc100/ptxconfig_generic
+ptxdist platform configs/cc100/platformconfig
+ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin
+ptxdist go -q

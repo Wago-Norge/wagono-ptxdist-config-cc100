709cc3c759592720ee5266241833d66e61fd990b
Thomas Sandberg <thomas@sandberg.cloud>
2023-02-10 14:23:53 +0100

    modified script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+cd /workspaces/cc100
 ptxdist select configs/cc100/ptxconfig_generic
 ptxdist platform configs/cc100/platformconfig
 ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin

ba4b0f50b0d58c3dd70ac28c964ea3d86d2d8e24
Thomas Sandberg <thomas@sandberg.cloud>
2023-03-14 07:24:19 +0100

    Added symlink creation

--- init.sh
+++ init.sh
@@ -1,1 +1,5 @@
 git submodule update --init
+cd /workspaces/cc100
+ptxdist select configs/cc100/ptxconfig_generic
+ptxdist platform configs/cc100/platformconfig
+ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin
